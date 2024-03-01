280e45fe552546f055ce47d80f852e675e6f4eba
Youssif Taha <youssif5655@gmail.com>
2024-03-01 07:59:04 +0200

    - implmented the login data and logic successfully - added the login_repo and implmented the logic in login_repo_impl - created login cubit and states - registered singelton for loginRepoImpl in dependency_injection - wraped the loginScreen with bloc brovider and provided the cubit (loginCubit) - used the registered singelton for cubit arg

--- lib/features/signup/data/repos/signup_repo.dart
+++ lib/features/signup/data/repos/signup_repo.dart
@@ -7,12 +7,4 @@ abstract class SignUpRepo {
     required String email,
     required String password,
   });
-
-  Future<void> createUser({
-    required String email,
-    required String fullName,
-    required String phone,
-    required String uId,
-    required String userType,
-  });
 }
