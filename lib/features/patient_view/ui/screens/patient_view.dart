7ed05d1b473fbac0f6c6cf23f2f1422017e6eb70
Youssif Taha <youssif5655@gmail.com>
2024-03-08 03:56:09 +0200

    added some of the UI components to the main screen

--- lib/features/patient_view/ui/screens/patient_view.dart
+++ lib/features/patient_view/ui/screens/patient_view.dart
@@ -1,18 +1,52 @@
+import 'package:antenatal_app/core/Helpers/spacing.dart';
+import 'package:antenatal_app/core/theming/colors.dart';
+import 'package:antenatal_app/core/theming/styles_manager.dart';
+import 'package:antenatal_app/features/home/ui/widgets/home_drawer.dart';
+import 'package:antenatal_app/features/home/ui/widgets/horizontal_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PatientView extends StatelessWidget {
   const PatientView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-        child: Column(
-          children: [],
-        ),
-      ),
+    return SafeArea(
+      child: Scaffold(
+          drawer: const HomeDrawer(),
+          appBar: AppBar(
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  icon: const Icon(Icons.menu),
+                );
+              },
+            ),
+            automaticallyImplyLeading: false,
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0.w),
+                  child: Text(
+                    'Hello! Hamza',
+                    style: getBoldStyle(
+                        color: ColorManger.darkPrimary, fontSize: 18.sp),
+                  ),
+                ),
+                verticalSpace(20),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+                  child: const HorizontalCalendar(),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
